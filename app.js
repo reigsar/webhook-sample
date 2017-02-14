d623fda52d18835f234e475846648321a80d62e8
SARG <sara.reig@gft.com>
2017-02-14 16:53:51 +0100

    correct message without blanks

--- app.js
+++ app.js
@@ -52,5 +52,5 @@ restService.post('/webhook', function (req, res) {
 });
 
 restService.listen((process.env.PORT || 5000), function () {
-    console.log("Webhook    server listening");
+    console.log("Webhook server listening");
 });
